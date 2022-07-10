8b0df45917b6a112c1b403bd5105a454acdd7407
zhijiejia <43838913+zhijiejia@users.noreply.github.com>
2022-07-10 14:28:03 +0800

    Update pspnet.py

--- pspnet.py
+++ pspnet.py
@@ -6,32 +6,28 @@ from utils import CE_Loss
 from torchvision import models
 from resnet import resnet50
 
+class PSPModule(nn.Module):
+    def __init__(self, in_channels, pool_sizes):
+        super().__init__()
+        reduction_channels = in_channels // len(pool_sizes)
+        self.stages = nn.ModuleList([self._make_stages(in_channels, reduction_channels, pool_size) for pool_size in pool_sizes])
 
-
-class _PSPModule(nn.Module):
-    def __init__(self, in_channels, pool_sizes, norm_layer=nn.BatchNorm2d):
-        super(_PSPModule, self).__init__()
-        out_channels = in_channels // len(pool_sizes)
-        self.stages = nn.ModuleList([self._make_stages(in_channels, out_channels, pool_size, norm_layer) for pool_size in pool_sizes])
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(in_channels + (out_channels * len(pool_sizes)), out_channels, kernel_size=3, padding=1, bias=False),
-            norm_layer(out_channels),
+    def _make_stages(self, in_channels, out_channels, bin_sz):
+        return nn.Sequential(
+            nn.AdaptiveAvgPool2d(output_size=bin_sz),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Dropout2d(0.1)
         )
 
-    def _make_stages(self, in_channels, out_channels, bin_sz, norm_layer=nn.BatchNorm2d):
-        prior = nn.AdaptiveAvgPool2d(output_size=bin_sz)
-        conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-        bn = norm_layer(out_channels)
-        relu = nn.ReLU(inplace=True)
-        return nn.Sequential(prior, conv, bn, relu)
-
     def forward(self, features):
-        h, w = features.size()[2], features.size()[3]
+        
         pyramids = [features]
-        pyramids.extend([F.interpolate(stage(features), size=(h, w), mode='bilinear', align_corners=True) for stage in self.stages])
-        output = self.bottleneck(torch.cat(pyramids, dim=1))
+        for stage in self.stages:
+            pyramids.append(
+                F.interpolate(stage(features), size=features.shape[2:], mode='bilinear', align_corners=True)
+            )
+        output = torch.cat(pyramids, dim=1)
         return output
 
 
@@ -39,32 +35,58 @@ class PSPNet(nn.Module):
     def __init__(self, num_classes):
         super(PSPNet, self).__init__()
 
-        self.backbone = resnet50(pretrained=True)
+        resnet = resnet50(pretrained=True)
+        self.layer0 = nn.Sequential(
+            resnet.conv1,
+            resnet.bn1,
+            resnet.relu, 
+            resnet.conv2,
+            resnet.bn2, 
+            resnet.relu, 
+            resnet.conv3, 
+            resnet.bn3, 
+            resnet.relu, 
+            resnet.maxpool,
+        )
+
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+
+        for n, m in self.layer3.named_modules():    
+            if 'conv2' in n:
+                m.dilation, m.padding, m.stride = (2, 2), (2, 2), (1, 1)
+            elif 'downsample.0' in n:             # 取消下采样, PSPNet默认使用的Resnet的下采样率是8
+                m.stride = (1, 1)
+        for n, m in self.layer4.named_modules():
+            if 'conv2' in n:
+                m.dilation, m.padding, m.stride = (4, 4), (4, 4), (1, 1)
+            elif 'downsample.0' in n:             # 取消下采样, PSPNet默认使用的Resnet的下采样率是8
+                m.stride = (1, 1)
+
         out_channel = 2048
 
-        self.master_branch = nn.Sequential(
-            _PSPModule(out_channel, pool_sizes=[1, 2, 3, 6]),
-            nn.Conv2d(out_channel // 4, num_classes, kernel_size=1)
+        self.pmp = PSPModule(out_channel, pool_sizes=[1, 2, 3, 6])
+        
+        self.cls = nn.Sequential(
+            nn.Conv2d(out_channel * 2, 512, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(512, num_classes, kernel_size=1, padding=1, bias=False)
         )
-        self.initialize_weights(self.master_branch)
 
     def forward(self, x):
-        input_size = (x.size()[2], x.size()[3])
-        x = self.backbone(x)
+        input_size = x.shape[2:]
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
-        output = self.master_branch(x)
+        output = self.pmp(x)
+        output = self.cls(output)
         output = F.interpolate(output, size=input_size, mode='bilinear', align_corners=True)
 
         return output
-
-    def initialize_weights(self, *models):
-        for model in models:
-            for m in model.modules():
-                if isinstance(m, nn.Conv2d):
-                    nn.init.kaiming_normal_(m.weight.data, nonlinearity='relu')
-                elif isinstance(m, nn.BatchNorm2d):
-                    m.weight.data.fill_(1.)
-                    m.bias.data.fill_(1e-4)
-                elif isinstance(m, nn.Linear):
-                    m.weight.data.normal_(0.0, 0.0001)
-                    m.bias.data.zero_()
